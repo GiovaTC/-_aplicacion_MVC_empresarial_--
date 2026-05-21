2d1e97eef29a11428d30fcdee66d554377c2aa86
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-21 08:58:01 -0500

    commit - AppDbContext .

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaMVCOracle.Models;
+
+namespace SistemaMVCOracle.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+        }
+        public DbSet<Producto> Produtos { get; set; }
+    }
+}   
