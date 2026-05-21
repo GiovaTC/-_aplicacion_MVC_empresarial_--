6de7a1b921ad71b5c623e0602f3e34491ef0f3c3
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-21 09:14:09 -0500

    commit - ProductosController / .

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaMVCOracle.Data;
+using SistemaMVCOracle.Models;
+
+
+namespace SistemaMVCOracle.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Productos.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                producto.FECHA_REGISTRO = DateTime.Now;
+
+                _context.Productos.Add(producto);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(producto);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+
+            return View(producto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Producto producto)
+        {
+            _context.Productos.Update(producto);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+
+            _context.Productos.Remove(producto);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}   
