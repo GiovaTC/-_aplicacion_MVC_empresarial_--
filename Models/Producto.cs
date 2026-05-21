bfacd79830e20139605a3f521de4e8d0e92c6e84
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-21 08:55:15 -0500

    commit - Producto .

---
+++ Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaMVCOracle.Models
+{
+    [Table("PRODUCTOS_O")]
+    public class Producto
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        public string NOMBRE { get; set; }
+        
+        public decimal PRECIO { get; set; }
+
+        public int STOCK { get; set; }
+
+        public DateTime FECHA_REGISTRO { get; set; }
+    }   
+}   
